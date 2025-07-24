ed5c3dc005d5b1179430252e10fc1ce602071772
Khush786Mohammad <khush786moh@gmail.com>
2025-07-24 16:23:49 +0000

    controller

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,7 +1,14 @@
 package com.example.demo;
+import java.util.*;
 
 public class controller {
     controller(){
-        
+
+    }
+}
+
+class student{
+    public static void main(string[] args){
+        System.out.println("Hello word");
     }
 }
